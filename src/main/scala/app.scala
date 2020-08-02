6f96ddc750a0ed1622168f7a5751dfde99ccca9d
fffixed <692565@gmail.com>
2020-08-02 23:55:36 +0300

    add chapter 05 - exercises 01-03

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -7,9 +7,11 @@ object app {
     //ch04.ex05()
     //ch04.ex06()
     //ch04.ex07()
-    println(ch04.minmax(Array(1,2,3,-4,5,6,0))) //ex08
+    //println(ch04.minmax(Array(1,2,3,-4,5,6,0))) //ex08
     //println(ch04.lteqgt(Array(1,2,3,-4,5,6,0,1,2,3,4,5,6), 0)) //ex09
     //ch04.ex10()
+    val q = new ch05.Time(10,20)
+    println(q.before(new ch05.Time(10,25)))
   }
 
 
---
+++ src/main/scala/ch05.scala
@@ -0,0 +1,38 @@
+object ch05 {
+
+  /** ex01 */
+  class Counter {
+    private var value = 0
+    def increment() { if (value == Int.MaxValue) 0 else value += 1 }
+    def current = value
+  }
+
+  /** ex02 */
+  class BankAccount {
+    private var _balance: Double = 0
+    def balance = _balance
+
+    def deposit(amount: Double) {
+      if (amount < 0) throw new Exception("Deposit amount can't be negative")
+      _balance += amount
+    }
+
+    def withdraw(amount: Double) {
+      if (amount < 0) throw new Exception("Withdrawal amount can't be negative")
+      _balance -= amount
+    }
+  }
+
+  /** ex03 */
+  class Time(val hours: Int, val minutes: Int) {
+    if (hours < 0 || hours > 23) throw new Exception("Incorrect hours value")
+    if (minutes < 0 || minutes > 59) throw new Exception("Incorrect minutes value")
+    def before(other: Time): Boolean = {
+      hours < other.hours  || (hours == other.hours && minutes < other.minutes)
+    }
+  }
+
+
+
+
+}
